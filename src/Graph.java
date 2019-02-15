0c4d88a8a6f81a7816b12f7a52dba60f5b2e12cb
PhillBenoit <PhillBenoit@users.noreply.github.com>
2019-02-15 01:55:37 -0700

    late night commit

--- src/Graph.java
+++ src/Graph.java
@@ -1,61 +1,90 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Graph {
     
+	private ArrayList<ArrayList<Integer>> graph;
+	
+	private final int NUMBER_OF_VERTICIES;
+	
     public Graph (int numVertices) {
-        
+        NUMBER_OF_VERTICIES = numVertices;
+    	graph = new ArrayList<ArrayList<Integer>>();
+        for (int step = 0; step < NUMBER_OF_VERTICIES; step++)
+        	graph.add(new ArrayList<Integer>());
     }
     
     //Returns a list of the ID#s of the inactive vertices of the graph. If there
     //are no inactive vertices, a reference to an empty list object is returned.
     public List<Integer> getUnusedVertices () {
-        
+        ArrayList<Integer> inactiveIntegers = new ArrayList<Integer>();
+        for (int step = 0;step < NUMBER_OF_VERTICIES; step++)
+        	if (graph.get(step).isEmpty()) inactiveIntegers.add(step);
+        return inactiveIntegers;
     }
     
     //Returns true if the graph possesses an edge directly connecting the given
     //source and destination vertices. That is, ‘true’ is returned if these
     //vertices are adjacent.
     public boolean isEdge (int source, int destination) {
-        
+        return graph.get(source).contains(destination);
     }
     
     //Ensures that the graph contains an edge connecting the given source and
     //destination vertices.
     public void addEdge (int source, int destination) {
-        
+        graph.get(source).add(destination);
+        graph.get(destination).add(source);
     }
     
     //Ensures that the graph does not contain an edge connecting the given
     //source and destination vertices.
     public void removeEdge (int source, int destination) {
-        
+        graph.get(source).remove((Integer)destination);
+        graph.get(destination).remove((Integer)source);
     }
     
     //Returns true if the given vertex is active within the graph.
     public boolean isInGraph (int vertex) {
-        
+        return !graph.get(vertex).isEmpty();
     }
     
     //Called to mark a vertex as inactive. Inactive vertices have no neighbors,
     //and thus have a degree of 0.
     public void removeVertex (int vertex) {
-        
+        graph.get(vertex).clear();
     }
     
     //Returns a list of vertex ID#s. A vertex is in the list if it is active
     //and adjacent to the given vertex.
     public List<Integer> getAdjacent (int vertex) {
-        
+        return graph.get(vertex);
     }
     
     //Returns the degree of the given vertex.
     public int degree (int vertex) {
-        
+    	return graph.get(vertex).size();
     }
+    
+    private boolean[] marked;
     
     //Returns true if the graph is connected; that is, if every active vertex
     //is reachable from every other active vertex.
     public boolean connected () {
-        
+        marked = new boolean[NUMBER_OF_VERTICIES];
+        int index = 0;
+        while (graph.get(index).isEmpty()) index++;
+        traverseGraph(index);
+        for (; index < NUMBER_OF_VERTICIES; index++)
+        	if (!marked[index] && !graph.get(index).isEmpty())
+        		return false;
+        return true;
+    }
+    
+    private void traverseGraph(int index) {
+    	marked[index] = true;
+    	for (Integer i:graph.get(index))
+    		if (!marked[i]) traverseGraph(i);
     }
     
 }
--- src/Map.java
+++ src/Map.java
@@ -1,127 +1,139 @@
 import java.util.ArrayList;
 
 public class Map {
-    
-    public final int ROWS, COLUMNS, //Size of the board.
-    VICTIMS, //Number of unused (‘invisible’) territories.
-    NUMTERRITORIES, //Synonym for ROWS times COLUMNS.
-    OCCUPIED, //Synonym for NUMTERRITORIES minus VICTIMS.
-    MAXDICE; //The largest quantity of dice a territory may have.
-    
-    private ArrayList<Player> players;
-    
-    private Territory[][] map;
-    
-    private Graph neighbor_graph;
-    
-    public Map (ArrayList<Player> players, int rows, int columns, int victims, int maxDice) {
-        this.players = players;
-        ROWS = rows;
-        COLUMNS = columns;
-        VICTIMS = victims;
-        MAXDICE = maxDice;
-        NUMTERRITORIES = ROWS * COLUMNS;
-        OCCUPIED = NUMTERRITORIES - VICTIMS;
-        map = new Territory[ROWS][COLUMNS];
-        int id_counter = 0;
-        for (Territory[] row: map)
-            for (Territory t: row)
-                t = new Territory(this, null, maxDice, id_counter++);
-        
-    }
-    
-    public Territory[][] getMap () {return map;}
 
-    public Graph getGraph () {return neighbor_graph;}
-    
-    public Territory getTerritory (int row, int column) {return map[row][column];}
-    
-    //: Given the row and column of a territory, compute and return the territory’s
-    //ID#. The Territory class supplies getRow() and getCol() methods; see
-    //“The Supplied Classes” section, below.
-    public int getTerritoryId (int row, int column){
-        return (row * COLUMNS) + column;
-    }
-    
-    //: Determine and return the quantity of territories owned by the given player.
-    public int countTerritories (Player player) {
-        int counter = 0;
-        for (Territory[] row: map)
-            for (Territory t: row)
-                if (t.getOwner() == player) counter++;
-        return counter;        
-    }
-    
-    //Determine and return the total number of dice currently assigned to this player’s territories.
-    public int countDice (Player player) {
-        int counter = 0;
-        for (Territory[] row: map)
-            for (Territory t: row)
-                if (t.getOwner() == player) counter += t.getDice();
-        return counter;        
-    }
-    
-    //Construct and return a reference to an ArrayList of Territory object
-    //references. The territories referenced by the list are those
-    //currently owned by the given player.
-    public ArrayList<Territory> getPropertyOf(Player player) {
-        ArrayList<Territory> list = new ArrayList<>();
-        for (Territory[] row: map)
-            for (Territory t: row)
-                if (t.getOwner() == player) list.add(t);
-        return list;        
-    }
+	public final int ROWS, COLUMNS, //Size of the board.
+	VICTIMS, //Number of unused (‘invisible’) territories.
+	NUMTERRITORIES, //Synonym for ROWS times COLUMNS.
+	OCCUPIED, //Synonym for NUMTERRITORIES minus VICTIMS.
+	MAXDICE; //The largest quantity of dice a territory may have.
 
-    //Each territory has at least one adjacent (edge-sharing) neighboring
-    //territory, but no more than four. This method returns a reference to an
-    //ArrayList of references to the given territory’s neighbors. The Graph object
-    //offers a helpful method: isInGraph(). isInGraph() takes a territory ID# and
-    //returns true if the territory is participating in the game (remember, some
-    //territories are ‘invisible’ and thus can’t be neighbors).
-    public ArrayList<Territory> getNeighbors(Territory cell) {
-        
-    }
-    
-    //Similar to getNeighbors(), above, but the returned list contains only
-    //references to neighboring territories controlled by another player.
-    public ArrayList<Territory> getEnemyNeighbors(Territory cell) {
-        
-    }
-    
-    //This method is called by the constructor after the array of un-owned
-    //territories has been built. This method assigns to each player the same
-    //(or nearly the same) quantity of territories. Each player’s territories
-    //are to be selected randomly. That is, do not assign players to territories
-    //based on a pattern.
-    private void partitionTerritories() {
-        
-    }
-    
-    //This method is called by the constructor after partitionTerritories() has
-    //been called. Collectively, the assigned territories of each player have
-    //the same quantity of dice as the territories of every other player: three
-    //times the player’s number of territories. For example, if there were three
-    //players and 15 territories per player, each player would start with 45 dice.
-    //This method randomly distributes each player’s dice allotment across his or
-    //her territories. Each territory must have at least one die, but no territory
-    //can have more than MAXDICE dice.
-    private void distributeDice() {
-        
-    }
-    
-    //Returns a count of the number of territories in the largest connected cluster
-    //of territories owned by the given player.
-    public int countConnected (Player player) {
-        
-    }
-    
-    //Builds and returns a reference to a graph representing all of the active
-    //territories in the game. An acceptable graph has the appropriate number
-    //of active territories (Map.OCCUPIED), ‘scatters’ the inactive territories
-    //among the active territories in an unpredictable (pseudo-random) fashion,
-    //and ensures that all of the active vertices are connected.
-    public Graph constructGraph(int rows, int cols, int victims) {
-        
-    }
+	private ArrayList<Player> players;
+
+	private Territory[][] map;
+
+	private Graph neighbor_graph;
+
+	public Map (ArrayList<Player> players, int rows, int columns, int victims, int maxDice) {
+		this.players = players;
+		ROWS = rows;
+		COLUMNS = columns;
+		VICTIMS = victims;
+		MAXDICE = maxDice;
+		NUMTERRITORIES = ROWS * COLUMNS;
+		OCCUPIED = NUMTERRITORIES - VICTIMS;
+		map = new Territory[ROWS][COLUMNS];
+		int id_counter = 0;
+		for (Territory[] row: map)
+			for (Territory t: row)
+				t = new Territory(this, null, maxDice, id_counter++);
+
+	}
+
+	public Territory[][] getMap () {return map;}
+
+	public Graph getGraph () {return neighbor_graph;}
+
+	public Territory getTerritory (int row, int column) {return map[row][column];}
+
+	private Territory geTerritory (int index) {
+		return getTerritory(index/COLUMNS, index%COLUMNS);
+	}
+
+	//: Given the row and column of a territory, compute and return the territory’s
+	//ID#. The Territory class supplies getRow() and getCol() methods; see
+	//“The Supplied Classes” section, below.
+	public int getTerritoryId (int row, int column){
+		return (row * COLUMNS) + column;
+	}
+
+	//: Determine and return the quantity of territories owned by the given player.
+	public int countTerritories (Player player) {
+		int counter = 0;
+		for (Territory[] row: map)
+			for (Territory t: row)
+				if (t.getOwner() == player) counter++;
+		return counter;        
+	}
+
+	//Determine and return the total number of dice currently assigned to this player’s territories.
+	public int countDice (Player player) {
+		int counter = 0;
+		for (Territory[] row: map)
+			for (Territory t: row)
+				if (t.getOwner() == player) counter += t.getDice();
+		return counter;        
+	}
+
+	//Construct and return a reference to an ArrayList of Territory object
+	//references. The territories referenced by the list are those
+	//currently owned by the given player.
+	public ArrayList<Territory> getPropertyOf(Player player) {
+		ArrayList<Territory> list = new ArrayList<>();
+		for (Territory[] row: map)
+			for (Territory t: row)
+				if (t.getOwner() == player) list.add(t);
+		return list;        
+	}
+
+	//Each territory has at least one adjacent (edge-sharing) neighboring
+	//territory, but no more than four. This method returns a reference to an
+	//ArrayList of references to the given territory’s neighbors. The Graph object
+	//offers a helpful method: isInGraph(). isInGraph() takes a territory ID# and
+	//returns true if the territory is participating in the game (remember, some
+	//territories are ‘invisible’ and thus can’t be neighbors).
+	public ArrayList<Territory> getNeighbors(Territory cell) {
+		ArrayList<Territory> return_list = new ArrayList<Territory>();
+		for (Integer i:neighbor_graph.getAdjacent(cell.getIdNum()))
+			return_list.add(geTerritory(i));
+		return return_list;
+	}
+
+	//Similar to getNeighbors(), above, but the returned list contains only
+	//references to neighboring territories controlled by another player.
+	public ArrayList<Territory> getEnemyNeighbors(Territory cell) {
+		ArrayList<Territory> return_list = new ArrayList<Territory>();
+		for (Integer i:neighbor_graph.getAdjacent(cell.getIdNum())) {
+			Territory t = geTerritory(i);
+			if (t.getOwner()!=cell.getOwner()) return_list.add(t);
+		}
+		return return_list;
+	}
+
+	//This method is called by the constructor after the array of un-owned
+	//territories has been built. This method assigns to each player the same
+	//(or nearly the same) quantity of territories. Each player’s territories
+	//are to be selected randomly. That is, do not assign players to territories
+	//based on a pattern.
+	private void partitionTerritories() {
+		
+	}
+
+	//This method is called by the constructor after partitionTerritories() has
+	//been called. Collectively, the assigned territories of each player have
+	//the same quantity of dice as the territories of every other player: three
+	//times the player’s number of territories. For example, if there were three
+	//players and 15 territories per player, each player would start with 45 dice.
+	//This method randomly distributes each player’s dice allotment across his or
+	//her territories. Each territory must have at least one die, but no territory
+	//can have more than MAXDICE dice.
+	private void distributeDice() {
+
+	}
+
+	//Returns a count of the number of territories in the largest connected cluster
+	//of territories owned by the given player.
+	public int countConnected (Player player) {
+		
+	}
+
+	//Builds and returns a reference to a graph representing all of the active
+	//territories in the game. An acceptable graph has the appropriate number
+	//of active territories (Map.OCCUPIED), ‘scatters’ the inactive territories
+	//among the active territories in an unpredictable (pseudo-random) fashion,
+	//and ensures that all of the active vertices are connected.
+	public Graph constructGraph(int rows, int cols, int victims) {
+
+	}
 
 }
