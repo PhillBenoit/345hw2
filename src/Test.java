15144b5c25ac5705464cc50b460ab131021f11eb
PhillBenoit <PhillBenoit@users.noreply.github.com>
2019-02-19 22:44:53 -0700

    base file refinements

--- src/Test.java
+++ src/Test.java
@@ -10,8 +10,13 @@ public class Test {
         players.add(new Player("Larry", new Color(0x00FF00)));
         players.add(new Player("Curly", new Color(0x0000FF)));
         
-        Map m = new Map(players, 5, 10, 5, 8);
-        System.out.println(m.NUMTERRITORIES);
+        int rows = 5, cols = 10;
+        Map m = new Map(players, rows, cols, 20, 8);
+        for (int x = 0; x < rows; x++) {
+        	for (int y = 0; y < cols; y++)
+        		System.out.print(m.getNeighbors(m.getTerritory(x, y)).isEmpty()? " " : "#");
+        	System.out.println();
+        }
     }
 
 }
